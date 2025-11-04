13f4c16211cf2c3a23d78273715cb7a645428a86
pageweb-2024 <152339341+pageweb-2023@users.noreply.github.com>
2025-11-04 17:44:41 -0500

    subir proyecto

---
+++ holamundo.py
@@ -0,0 +1,17 @@
+print("hola mundo")
+
+numero1 = 25
+numero2 = 45
+
+resultadoSuma = numero1 + numero2
+resultadoresta = numero2 - numero2
+resultadomultiplicacion = numero1 * numero2
+resultadodivision = numero1 / numero2
+
+print("el resultado de la suma es: ", resultadoSuma)
+
+print("el resultado de la resta es: ", resultadoresta)
+
+print("el resultado de la multiplicacion es: ", resultadomultiplicacion)
+
+print("el resultado de la division es: ", resultadodivision)
